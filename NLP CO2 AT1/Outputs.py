91132e03937f550764d90a78e05c57b6028e0ec5
JabastianR <192472138.simats@saveetha.com>
2026-07-31 08:56:49 +0530

    Add files via upload

---
+++ NLP CO2 AT1/Outputs.py
@@ -0,0 +1,42 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/cravi/OneDrive/Desktop/Jabastian college/NLP folder/NLP CO2 AT1/question 1.py
+----------------------------------------------------------------------
+Word            Root         Suffix     Type            Normalized     
+----------------------------------------------------------------------
+connected       connect      ed         Inflectional    connect        
+connecting      connect      ing        Inflectional    connect        
+connection      connect      ion        Derivational    connect        
+
+= RESTART: C:/Users/cravi/OneDrive/Desktop/Jabastian college/NLP folder/NLP CO2 AT1/question 2.py
+-------------------------------------------------------------------------------------
+Word         Prefix     Root       Suffix     Type            Normalized
+-------------------------------------------------------------------------------------
+unhappy      un         happy      -          Derivational    happy     
+happiness    -          happy      ness       Derivational    happy     
+happily      -          happy      ly         Derivational    happy     
+>>> 
+= RESTART: C:/Users/cravi/OneDrive/Desktop/Jabastian college/NLP folder/NLP CO2 AT1/question 3.py
+---------------------------------------------------------------------------
+Word         Stem       Removed Affix   Type            Normalized
+---------------------------------------------------------------------------
+played       play       ed              Inflectional    play      
+player       play       er              Derivational    play      
+playing      play       ing             Inflectional    play      
+>>> 
+================= RESTART: C:/Users/cravi/OneDrive/Desktop/Jabastian college/NLP folder/NLP CO2 AT1/question 4.py =================
+------------------------------------------------------------------------------------------
+Word         State Path           Root       Type            Normalized
+------------------------------------------------------------------------------------------
+writes       q0 -> q1 -> q2       write      Regular         write     
+writing      q0 -> q1 -> q3       write      Regular         write     
+written      q0 -> q4             write      Irregular       write     
+>>> 
+================= RESTART: C:/Users/cravi/OneDrive/Desktop/Jabastian college/NLP folder/NLP CO2 AT1/question 5.py =================
+--------------------------------------------------------------------------------
+Word            Rule Applied              Intermediate         Final Stem     
+--------------------------------------------------------------------------------
+relational      Removed "ional"           relate               relat          
+relation        Removed "ion"             relate               relat          
+relate          Removed "e"               relat                relat          
